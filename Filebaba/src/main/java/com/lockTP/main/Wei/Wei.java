f6c384e9c9eed93a45d6c8823e1b412ffe4d672a
wed26 <weidai@Weis-MacBook-Pro.local>
2015-01-25 16:18:31 -0500

    test

--- Filebaba/src/main/java/com/lockTP/main/Wei/Wei.java
+++ Filebaba/src/main/java/com/lockTP/main/Wei/Wei.java
@@ -1,5 +1,10 @@
 package com.lockTP.main.Wei;
 
 public class Wei {
+	public int add(int a, int b)
+	{
+		int c = a+b;
+		return c;
+	}
 
 }
--- Filebaba/src/test/java/com/lockTP/main/Wei/WeiTest.java
+++ Filebaba/src/test/java/com/lockTP/main/Wei/WeiTest.java
@@ -1,7 +1,15 @@
 package com.lockTP.main.Wei;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class WeiTest extends TestCase {
+	@Test
+	public void testAdd(){
+		Wei wei = new Wei();
+		int c = wei.add(3, 2);
+		assertEquals(8,c);
+	}
 
 }
