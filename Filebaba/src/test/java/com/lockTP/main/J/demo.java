7f794ed05d8daf301b67c7748da9dc6e9e3b99fd
jpotato0o <1546232650@qq.com>
2015-01-25 12:45:47 -0500

    change

---
+++ Filebaba/src/test/java/com/lockTP/main/J/demo.java
@@ -0,0 +1,7 @@
+package com.lockTP.main.J;
+
+import junit.framework.TestCase;
+
+public class demo extends TestCase {
+
+}
--- Filebaba/src/test/java/com/lockTP/main/main/J/test.java
+++
@@ -1,7 +0,0 @@
-package com.lockTP.main.main.J;
-
-import junit.framework.TestCase;
-
-public class test extends TestCase {
-
-}
